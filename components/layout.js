48277624020b2375d8bd23bcc9c61c3dce34f342
Usama Israr <156895405+Usama1190@users.noreply.github.com>
2024-08-02 01:15:34 -0700

    Keep Learning on Aug 02, 24

---
+++ components/layout.js
@@ -0,0 +1,6 @@
+import Style from './layout.module.css';
+
+
+export default function Layout({ children }) {
+    return <div className='styles.container'>{children}</div>;
+}
--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,9 +1,10 @@
 import Link from 'next/link';
 import Head from 'next/head';
 import Script from 'next/script';
+import Layout from '../../components/layout';
 
 export default function FirstPost() {
-    return <>
+    return <Layout>
         <Head>
             <title>First Post</title>
         </Head>
@@ -17,5 +18,5 @@ export default function FirstPost() {
         
         <h1>First Post!</h1>
         <h2><Link href="/">Home</Link></h2>
-    </>
+    </Layout>
 }
